f5946d5f4e5fdbdb9b6e292009b49aec1451f37f
Pool <pool_1541@hotmail.com>
2023-09-26 19:44:16 -0500

    feat(user): add middleware class for user entity

--- src/shared/routes/router.ts
+++ src/shared/routes/router.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
-export class BaseRouter<T> {
+export class BaseRouter<T, U = any> {
   public router: Router;
   public controller: T;
-  // public middleware: U;
+  public middleware?: U;
 
-  constructor(TController: { new (): T }) {
+  constructor(TController: { new (): T }, UMiddleware?: { new (): U }) {
     this.router = Router();
     this.controller = new TController();
+    if (UMiddleware) this.middleware = new UMiddleware();
     this.routes();
   }
 
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,9 +1,10 @@
 import { UserController } from './controllers/user.controller';
 import { BaseRouter } from '../shared/routes/router';
+import { UserMiddleware } from './middlewares/user.middleware';
 
-export class UserRouter extends BaseRouter<UserController> {
+export class UserRouter extends BaseRouter<UserController, UserMiddleware> {
   constructor() {
-    super(UserController);
+    super(UserController, UserMiddleware);
   }
 
   routes(): void {
@@ -12,7 +13,11 @@ export class UserRouter extends BaseRouter<UserController> {
     this.router.get('/userWithRelation/:id', (req, res) =>
       this.controller.getUserWithRelation(req, res)
     );
-    this.router.post('/user', (req, res) => this.controller.createUser(req, res));
+    this.router.post(
+      '/user',
+      (req, res, next) => [this.middleware?.userValidator(req, res, next)],
+      (req, res) => this.controller.createUser(req, res)
+    );
     this.router.put('/user/:id', (req, res) => this.controller.updateUser(req, res));
     this.router.delete('/user/:id', (req, res) => this.controller.deleteUser(req, res));
   }
